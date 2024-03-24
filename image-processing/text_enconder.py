9b0cb511884771fc281775a4e43b2559baf31f54
Mateus Orsoni <matorsoni45@gmail.com>
2024-03-24 20:32:02 -0300

    Text encode/decode working

--- image-processing/text_enconder.py
+++ image-processing/text_enconder.py
@@ -9,8 +9,8 @@ def char_into_bgr(c: np.uint8, bgr: np.array) -> np.array:
     r_bits = (c & 0b11100000) >> 5
 
     bgr[0] = (bgr[0] & 0b11111000) | b_bits
-    bgr[1] = (bgr[1] & 0b11100111) | g_bits
-    bgr[2] = (bgr[2] & 0b00011111) | r_bits
+    bgr[1] = (bgr[1] & 0b11111100) | g_bits
+    bgr[2] = (bgr[2] & 0b11111000) | r_bits
 
     return bgr
 
@@ -19,9 +19,9 @@ def bgr_into_char(bgr: np.array) -> np.uint8:
     assert bgr.shape == (3,)
 
     c = np.uint8(0)
-    c = (bgr[0] & 0b00000111 ) | c
-    c = (bgr[1] & 0b00011000 ) | c
-    c = (bgr[2] & 0b11100000 ) | c
+    c = (bgr[0] & 0b00000111)      | c
+    c = (bgr[1] & 0b00000011) << 3 | c
+    c = (bgr[2] & 0b00000111) << 5 | c
 
     return c
 
@@ -31,10 +31,10 @@ def text_into_image(text: str, image: np.array) -> None:
     # Create text header, which consists of 8 characters:
     # "TEXT" + 4 bytes representing the text length as an unsigned integer.
     len32 = np.uint32(len(text))
-    len8_0 = np.uint8(len32 & 0b00000011)
-    len8_1 = np.uint8((len32 & 0b00001100) >> 2)
-    len8_2 = np.uint8((len32 & 0b00110000) >> 4)
-    len8_3 = np.uint8((len32 & 0b11000000) >> 6)
+    len8_0 = np.uint8( len32 & 0b11111111)
+    len8_1 = np.uint8((len32 & (0b11111111 << 8)) >> 8)
+    len8_2 = np.uint8((len32 & (0b11111111 << 16)) >> 16)
+    len8_3 = np.uint8((len32 & (0b11111111 << 16)) >> 24)
     header = "TEXT" + chr(len8_3) + chr(len8_2) + chr(len8_1) + chr(len8_0)
 
     new_text = header + text
@@ -56,11 +56,18 @@ def read_text_from_image(image: np.array) -> str:
         chars.append(bgr_into_char(bgr))
     header_text = '' + chr(chars[0]) + chr(chars[1]) + chr(chars[2]) + chr(chars[3])
     text_length = np.uint32(0)
-    text_length = np.uint32(chars[4]) << 6 | text_length
-    text_length = np.uint32(chars[5]) << 4 | text_length
-    text_length = np.uint32(chars[6]) << 2 | text_length
-    text_length = np.uint32(chars[7])      | text_length
+    text_length = (np.uint32(chars[4]) << 24) | text_length
+    text_length = (np.uint32(chars[5]) << 16) | text_length
+    text_length = (np.uint32(chars[6]) << 8)  | text_length
+    text_length = np.uint32(chars[7])         | text_length
 
     print(header_text)
     print(text_length)
+
+    chars = []
+    text = ''
+    for c in range(8, text_length):
+        i,j = divmod(c, image.shape[1])
+        text += chr(bgr_into_char(image[i,j]))
+    print(text)
 
