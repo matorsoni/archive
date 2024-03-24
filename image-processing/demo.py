642454b6661381d24825a6d1cf0e9ba0026a96f1
Mateus Orsoni <matorsoni45@gmail.com>
2024-03-24 14:31:06 -0300

    Add argument parser to img encoder script

--- image-processing/demo.py
+++ image-processing/demo.py
@@ -1,8 +1,57 @@
+import argparse
+from pathlib import Path
+import sys
+
 import cv2
 
+def argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='encoder',
+        description='Encode text into an image.',
+    )
+
+    parser.add_argument(
+        '-i', '--input',
+        help="Input image file (.jpeg or .png)",
+        required=True,
+    )
+
+    parser.add_argument(
+        '-t', '--text',
+        help="Input text file (.txt)",
+        required=False,
+    )
+
+    return parser
 
 def main():
-    img = cv2.imread("nhe.png")
+    parser = argparser()
+    args = parser.parse_args()
+    img_path_str = args.input
+    text_path_str = args.text
+
+    # Check received arguments
+    img_path = Path(img_path_str)
+    if not img_path.exists() or not img_path.is_file():
+        print(f"Invalid input image file name: {img_path_str}")
+        sys.exit(1)
+
+    out_img_path_str = str(img_path.parent / f"encoded_{img_path.name}")
+
+    if text_path_str is None:
+        text_path_str = str(Path(__file__).parent / "default_text.txt")
+
+    text_path = Path(text_path_str)
+    if not text_path.exists() or not text_path.is_file():
+        print("Invalid input text file name: {text_path_str}")
+        sys.exit(1)
+
+    # Read inputs
+    img = cv2.imread(img_path_str)
+    text = text_path.read_text()
+    print(f"Input image: {img.shape[0]}x{img.shape[1]} pixels.")
+    print(f"Input text: {len(text)} characters.")
+
     r = img[:,:,2]
     g = img[:,:,1]
     b = img[:,:,0]
@@ -10,11 +59,12 @@ def main():
     # process channel red
     c = r
 
-    # save a binary image with each bit of the input grayscale 8bit channel
+    # Save a binary image with each bit of the input grayscale 8bit channel
     # (c & 0b00000001) * 255
     # (c & 0b00000010) * 127
     # (c & 0b00000100) * 63, etc...
-    # most significant bits get attenuated: max value go like (255,254,252,248,240,224,192,128)
+    #
+    # Note that most significant bits get attenuated: max values are (255,254,252,248,240,224,192,128)
     # but it's ok for now
     for i in range(8):
         cv2.imwrite( f"bit{i}_in.png", (c & (1 << i)) * (255 >> i) )
