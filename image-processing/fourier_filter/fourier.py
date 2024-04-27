12d46922c89a14997d0ef019adc258dcf1c483f5
Mateus Orsoni <matorsoni45@gmail.com>
2024-04-26 21:21:01 -0300

    Optmize masking with np.indices

--- image-processing/fourier_filter/fourier.py
+++ image-processing/fourier_filter/fourier.py
@@ -5,17 +5,26 @@ import sys
 import cv2
 import numpy as np
 
-def circle_mask(img: np.array, radius: int) -> np.array:
+def circle_mask(img: np.array, inner: int, outer: int, flip: bool) -> np.array:
     assert len(img.shape) == 2
-    cols, rows = img.shape
-    mask = np.zeros(img.shape, dtype = np.uint8)
-    mask_radius = int(radius/200.0 * min(rows, cols))
+    assert inner <= outer
+    rows, cols = img.shape
+    mask = np.ones(img.shape, dtype = bool)
+    if flip:
+        mask = ~mask
+
+    # Input values range from 0 to 100, so divide by 200 to get radius
+    inner_radius = int(inner/200.0 * min(rows, cols))
+    outer_radius = int(outer/200.0 * min(rows, cols))
     # TODO: Rewrite with np.indices(img.shape)
-    for i in range(rows):
-        for j in range(cols):
-            shifted_i = i-rows/2
-            shifted_j = j-cols/2
-            mask[i,j] = 1 if shifted_i*shifted_i + shifted_j*shifted_j <= mask_radius*mask_radius else 0
+    row_ind, col_ind = np.indices(img.shape, dtype=np.float32)
+    row_ind -= rows/2.0
+    col_ind -= cols/2.0
+    inner_mask = row_ind*row_ind + col_ind*col_ind <= inner_radius*inner_radius
+    outer_mask = row_ind*row_ind + col_ind*col_ind <= outer_radius*outer_radius
+    mask[outer_mask] = ~mask[outer_mask]
+    mask[inner_mask] = ~mask[inner_mask]
+
     return mask * img
 
 def main():
@@ -66,24 +75,31 @@ def main():
     window = "Fourier Filter"
     inner = "Inner radius"
     outer = "Outer radius"
+    switch = "FFT filter On/Off"
+    flip = "Flip filter On/Off"
     cv2.namedWindow(window)
     def do_nothing(x): pass   # Trackbar callback does not need to do anything
     cv2.createTrackbar(inner, window, 0, 100, do_nothing)
+    cv2.setTrackbarPos(inner, window, 100)
     cv2.createTrackbar(outer, window, 0, 100, do_nothing)
-
-    # Switch FFT On/Off.
-    switch = '0 : OFF \n1 : ON'
+    cv2.setTrackbarPos(outer, window, 100)
     cv2.createTrackbar(switch, window, 0, 1, do_nothing)
+    cv2.createTrackbar(flip, window, 0, 1, do_nothing)
 
     # Main loop.
     while(True):
         # Read value set on the UI
         inner_r = cv2.getTrackbarPos(inner, window)
         outer_r = cv2.getTrackbarPos(outer, window)
-        s = cv2.getTrackbarPos(switch, window)
+        switch_on = cv2.getTrackbarPos(switch, window)
+        flip_on = bool(cv2.getTrackbarPos(flip, window))
+
+        if inner_r > outer_r:
+            inner_r = outer_r
+            cv2.setTrackbarPos(inner, window, outer_r)
 
         # Update frequency domain image with binary mask and also the final image
-        masked_fft = img_fft if s == 0 else circle_mask(img_fft, inner_r)
+        masked_fft = img_fft if switch_on == 0 else circle_mask(img_fft, inner_r, outer_r, flip_on)
         filtered = 255*np.abs(np.fft.ifft2(np.fft.fftshift(masked_fft)))
 
         # Write images to the display and render
