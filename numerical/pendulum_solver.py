6a4e7261b8e5bfccaa1f9dc8216f1292bbe27114
matorsoni <matorsoni45@gmail.com>
2024-09-08 19:16:09 -0300

    Fix some issues on pendulum solver, still not converging :(

--- numerical/pendulum_solver.py
+++ numerical/pendulum_solver.py
@@ -6,8 +6,6 @@ Solve the non-linear pendulum EDO, where k = g/L, with Dirichlet conditions.
 
 '''
 
-
-
 alpha = 0.7
 beta = 0.7
 T = 2*np.pi
@@ -16,7 +14,6 @@ T = 2*np.pi
 # The recurrence m' = 2m+1 makes sure we are able to sample the exact same position for different m:
 #   U'_2i = U_i => 2i h' = ih => 2T/(m'+1) = T/(m+1) => m' = 2m+1
 M = [50]
-
 for i in range(8):
     M.append(2 * M[-1] + 1)
 
@@ -33,33 +30,32 @@ for r in range(1,m-1):
 A /= h2
 
 F = np.zeros(shape=(m,1), dtype=np.float64)
-F[0] = alpha / h2
-F[m-1] = beta / h2
+F[0][0] = alpha / h2
+F[m-1][0] = beta / h2
 
 def G(u):
-    return A@u + np.sin(u) + F
+    return A@u + np.reshape(np.sin(u), newshape=(m,1)) + F
 
 def J(u):
-    return A + np.diag(h2*np.cos(u))
+    return A + np.diagflat(h2*np.cos(u))
 
 t = h*(1.+np.array(range(m), dtype=np.float64))
-u0 = 0.7*np.cos(t) + 0.5*np.sin(t)
-U = [u0]
-for i in range(m):
-    u = U[-1]
+u0 = np.reshape(0.7*np.cos(t) + 0.5*np.sin(t), newshape=(m,1))
+u = u0
+U = []
+iters = 10
+for i in range(iters):
     delta = np.linalg.solve(J(u), -G(u))
-    U.append(u + delta)
+    print(f"Max norm of delta: {max(abs(delta))}")
+    u = u + delta
+    U.append(u)
 
 # Generate plot
 import matplotlib.pyplot as plt
-for i in range(m):
+for i in range(iters):
+    plt.xlabel('Time (t)')
+    plt.ylabel('Numerical solution U')
+    plt.ylim(-1,1)
+    plt.grid(False)
     plt.plot(t, U[i], 'ro-')
-#plt.plot(H, D_0_error, 'bs-')
-#plt.plot(H, D_3_error, 'g^-')
-plt.xlabel('h')
-plt.ylabel('Aproximation error')
-#plt.xscale('log')
-#plt.yscale('log')
-plt.grid(False)
-#plt.legend(["D+", "D0", "D3"], loc="lower right")
-plt.show()
+    plt.show()
