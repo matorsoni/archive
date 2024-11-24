24f661178b891663f5ed899b92be76a6382d05de
Mateus Orsoni <matorsoni45@gmail.com>
2024-11-24 16:25:28 -0300

    Create multiple files for simulation .dat

--- numerical/advection/advection.c
+++ numerical/advection/advection.c
@@ -4,6 +4,11 @@
 #define PI 3.141592653589793
 #define NX 100
 #define NT 100
+#define XMIN 0.0
+#define XMAX 2.0
+#define H ((XMAX - XMIN) / (NX - 1))
+#define K 0.01
+const double a = 1.0;
 
 double U[NT][NX];
 double X[NX];
@@ -13,39 +18,49 @@ double eta(double x) {
     return cond * 1.0 + !cond * 2.0;
 }
 
-int main() {
-    const char *filename = "output.dat";
-    const double x_min = 0.0;
-    const double x_max = 2.0;
-    const double h = (x_max - x_min) / (NX - 1);
-    const double k = 0.01;
+void upwind(int n) {
+    const double courant = a*K/H;
+    U[n][0] = U[n-1][0];
+    for (int i = 1; i < NX; ++i) {
+        U[n][i] = U[n-1][i] - courant * (U[n-1][i] - U[n-1][i-1]);
+    }
+}
 
+int main() {
     // Fill X values
-    X[0] = x_min;
+    X[0] = XMIN;
     for (int i = 1; i < NX; i++) {
-        X[i] = X[i-1] + h;
+        X[i] = X[i-1] + H;
     }
     // Fill initial condition u(x, t=0)
     for (int i = 0; i < NX; i++) {
         U[0][i] = eta(X[i]);
     }
 
-
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        perror("Error opening the file");
-        return -1;
+    // Run simulation
+    for (int n = 1; n < NT; ++n) {
+        upwind(n);
     }
 
-    // Write results to file
-    fprintf(file, "# X\tU\n");
-    for (int i = 0; i < NX; i++) {
-        fprintf(file, "%lf\t%lf\n", X[i], U[0][i]);
+    // Write results to files
+    for (int n = 0; n < NT; ++n) {
+        char filename[50];
+        snprintf(filename, 49, "output_%d.dat", n);
+        FILE *file = fopen(filename, "w");
+        if (file == NULL) {
+            perror("Error opening the file");
+            return -1;
+        }
+
+        fprintf(file, "# X\tU[n=%d]\n", n);
+        for (int i = 0; i < NX; i++) {
+            fprintf(file, "%lf\t%lf\n", X[i], U[n][i]);
+        }
+
+        fclose(file);
+        printf("File '%s' generated successfully.\n", filename);
     }
 
-    fclose(file);
-
-    printf("File '%s' generated successfully.\n", filename);
 
     return 0;
 }
