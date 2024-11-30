bb7b7b5d6b7e96dc238ba6b8e1bd6ec2c7620951
Mateus Orsoni <matorsoni45@gmail.com>
2024-11-30 13:00:10 -0300

    Adjust plot and script to show Lax-Friedrichs vs other methods

--- numerical/advection/python/burgers.py
+++ numerical/advection/python/burgers.py
@@ -14,17 +14,23 @@ X = np.linspace(ax, bx, m + 2)
 I = np.arange(1, m + 1)
 I_ = np.arange(1, m)
 
-# Initial condition
-def eta(X):
-    return 2.0 - 1. * (X >= 0.2).astype(float)  # Heaviside step function
+def step(X):
+    return 2.0 - 1. * (X >= 0.2).astype(float)
 
+def gauss(X):
+    var = 400
+    return np.exp(-var*(X-0.2)**2) + 1
+
+# Initial condition
+eta = step
+U_lax = eta(X)
 U = eta(X)
 
-# Main time-stepping loop
+# Run simulation with Dirichlet boundary conditions (reflective)
 for n in range(nsteps):
 
     # Regular upwind
-    #U[I] = U[I] - (k/h)*U[I]*(U[I] - U[I-1])
+    U[I] = U[I] - (k/h)*U[I]*(U[I] - U[I-1])
 
     # Delayed upwind
     #U[I] = U[I] - (k/h)*U[I-1]*(U[I] - U[I-1])
@@ -32,14 +38,15 @@ for n in range(nsteps):
     # Conservative Upwind
     #U[I] = U[I] - (k/(2*h)) * (U[I]**2 - U[I-1]**2)
 
-    # Lax-Friedrichs
-    U[I_] = 0.5 * (U[I_-1] + U[I_+1]) - (k/(4*h)) * (U[I_+1]**2 - U[I_-1]**2)
-
-    # Dirichlet boundary conditions (reflective)
     U[0] = U[1]
     U[-1] = U[-2]
 
+    # Lax-Friedrichs
+    U_lax[I_] = 0.5 * (U_lax[I_-1] + U_lax[I_+1]) - (k/(4*h)) * (U_lax[I_+1]**2 - U_lax[I_-1]**2)
+    U_lax[0] = U_lax[1]
+    U_lax[-1] = U_lax[-2]
+
 # Save data for plotting
 output_file = "output.dat"
-np.savetxt(output_file, np.c_[X, eta(X - k*nsteps), U], header="X FTRUE U", comments="")
+np.savetxt(output_file, np.c_[X, U_lax, U], header="X Lax-Friedrichs U", comments="")
 print(f"File '{output_file}' generated.")
--- numerical/advection/python/plot_burgers.gp
+++ numerical/advection/python/plot_burgers.gp
@@ -4,10 +4,12 @@ set xlabel "x"
 set ylabel "u(x,t)"
 set xrange [0:1]
 set yrange [0:3]
-set title "Numerical vs Analytical Solution at t=0.214s"
+set title "Numerical comparison: Lax-Friedrichs vs Regular Upwind at t=0.2s"
 set grid
-set style line 1 lc rgb "blue" lw 2 # Analytical solution style (blue line)
-set style line 2 lc rgb "red" pt 6 ps 1.5 # Numerical solution style (red circles)
 
-plot "output.dat" using 1:2 with lines linestyle 1 title "Analytical Solution", \
-     "output.dat" using 1:3 with points linestyle 2 title "Numerical Solution"
+# Styles: Blue line and red circles
+set style line 1 lc rgb "blue" lw 2
+set style line 2 lc rgb "red" pt 6 ps 1.5
+
+plot "output.dat" using 1:2 with lines linestyle 1 title "Lax-Friedrichs", \
+     "output.dat" using 1:3 with points linestyle 2 title "Regular Upwind"
