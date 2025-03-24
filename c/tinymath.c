e005c08098ab01c7bbba7c20ab6337dae0149737
Mateus Orsoni <matorsoni45@gmail.com>
2025-03-24 20:15:05 -0300

    Implement vec4f and mat4f basic operations

--- c/tinymath.c
+++ c/tinymath.c
@@ -1,123 +1,43 @@
 #include <stdio.h>
 
-typedef union {
-    struct { float x, y, z, w; };
-    float data[4];
-} vec4f;
+#include "tinymath.h"
 
-typedef union {
-    vec4f cols[4];
-    float data[16];
-} mat4f;
 
-#define EX4F (vec4f){.data = {1.0f, 0.0f, 0.0f, 0.0f}}
-#define EY4F (vec4f){.data = {0.0f, 1.0f, 0.0f, 0.0f}}
-#define EZ4F (vec4f){.data = {0.0f, 0.0f, 1.0f, 0.0f}}
-#define EW4F (vec4f){.data = {0.0f, 0.0f, 0.0f, 1.0f}}
-
-#define ID4F (mat4f){.cols = {EX4F, EY4F, EZ4F, EW4F}}
-#define M4F_ROW(A, i) (vec4f){.data = {(A)->data[0][(i)], (A)->data[1][(i)], (A)->data[2][(i)], (A)->data[3][(i)]}}
-
-void print_vec4f(const vec4f* a) {
-    printf("%.6f %.6f %.6f %.6f\n", a->x, a->y, a->z, a->w);
+void print_vec4f(const vec4f v) {
+    printf("[%.6f %.6f %.6f %.6f]\n", v.x, v.y, v.z, v.w);
 }
 
-void print_mat4f(const mat4f* A) {
+
+void print_mat4f(const mat4f A) {
     printf(
-        "[%.6f %.6f %.6f %.6f]\n" \
-        "[%.6f %.6f %.6f %.6f]\n" \
-        "[%.6f %.6f %.6f %.6f]\n" \
-        "[%.6f %.6f %.6f %.6f]\n",
-        A->data[0], A->data[4], A->data[8],  A->data[12],
-        A->data[1], A->data[5], A->data[9],  A->data[13],
-        A->data[2], A->data[6], A->data[10], A->data[14],
-        A->data[3], A->data[7], A->data[11], A->data[15]
+        "[[%.6f %.6f %.6f %.6f]\n" \
+        "[ %.6f %.6f %.6f %.6f]\n" \
+        "[ %.6f %.6f %.6f %.6f]\n" \
+        "[ %.6f %.6f %.6f %.6f]]\n",
+        A.elt[0], A.elt[4], A.elt[8],  A.elt[12],
+        A.elt[1], A.elt[5], A.elt[9],  A.elt[13],
+        A.elt[2], A.elt[6], A.elt[10], A.elt[14],
+        A.elt[3], A.elt[7], A.elt[11], A.elt[15]
     );
 }
 
-float reduce_vec4f(const vec4f* a) {
-    return a->x + a->y + a->z + a->w;
-}
-
-vec4f vecmul_vec4f(const vec4f* a, const vec4f* b) {
-    return (vec4f) {
-        .data = {a->x * b->x, a->y * b->y, a->z * b->z, a->w * b->w}
-    };
-}
-
-float dot_vec4f(const vec4f* a, const vec4f* b) {
-    vec4f mul = vecmul_vec4f(a,b);
-    return reduce_vec4f(&mul);
-}
-
-vec4f matmul_vec4f(const mat4f* A, const vec4f* b) {
-    return (vec4f) {
-        .x = dot_vec4f(&M4F_ROW(A, 0), b),
-        .y = dot_vec4f(&M4F_ROW(A, 1), b),
-        .z = dot_vec4f(&M4F_ROW(A, 2), b),
-        .w = dot_vec4f(&M4F_ROW(A, 3), b),
-    };
-}
-
-mat4f matmul_mat4f(const mat4f* A, const mat4f* B) {
-    return (mat4f) {
-        .cols = {
-            {
-                dot_vec4f(&M4F_ROW(A, 0), &B->cols[0]),
-                dot_vec4f(&M4F_ROW(A, 1), &B->cols[0]),
-                dot_vec4f(&M4F_ROW(A, 2), &B->cols[0]),
-                dot_vec4f(&M4F_ROW(A, 3), &B->cols[0]),
-            },
-            {
-                dot_vec4f(&M4F_ROW(A, 0), &B->cols[1]),
-                dot_vec4f(&M4F_ROW(A, 1), &B->cols[1]),
-                dot_vec4f(&M4F_ROW(A, 2), &B->cols[1]),
-                dot_vec4f(&M4F_ROW(A, 3), &B->cols[1]),
-            },
-            {
-                dot_vec4f(&M4F_ROW(A, 0), &B->cols[2]),
-                dot_vec4f(&M4F_ROW(A, 1), &B->cols[2]),
-                dot_vec4f(&M4F_ROW(A, 2), &B->cols[2]),
-                dot_vec4f(&M4F_ROW(A, 3), &B->cols[2]),
-            },
-            {
-                dot_vec4f(&M4F_ROW(A, 0), &B->cols[3]),
-                dot_vec4f(&M4F_ROW(A, 1), &B->cols[3]),
-                dot_vec4f(&M4F_ROW(A, 2), &B->cols[3]),
-                dot_vec4f(&M4F_ROW(A, 3), &B->cols[3]),
-            }
-        }
-    };
-}
-
 int main() {
-    print_vec4f(&(vec4f){.x = 1.0f, .y = 2.0f, .z = 3.0f, .w = 4.0f});
-    print_vec4f(&(vec4f){.data = {1.0f, 2.0f, 3.0f, 4.0f}});
-    print_vec4f(&(vec4f){{1.0f, 2.0f, 3.0f, 4.0f}});
+    print_vec4f((vec4f){.x = 1.0f, .y = 2.0f, .z = 3.0f, .w = 4.0f});
+    print_vec4f((vec4f){.elt = {1.0f, 2.0f, 3.0f, 4.0f}});
+    print_vec4f((vec4f){{1.0f, 2.0f, 3.0f, 4.0f}});
 
     printf("\n");
-    print_mat4f(&ID4F);
-    vec4f a = matmul_vec4f(&ID4F, &(vec4f){{1.0f, 2.0f, 3.0f, 4.0f}});
-    print_vec4f(&a);
+    print_mat4f(id4f());
+    vec4f a = mvmul4f(id4f(), (vec4f){{1.0f, 2.0f, 3.0f, 4.0f}});
+    print_vec4f(a);
 
     printf("\n");
-    print_mat4f(&(mat4f){.cols = {a, a, a, a}});
+    mat4f A = (mat4f){.col = {a, a, a, a}};
+    print_mat4f(A);
 
     printf("\n");
-    mat4f A = matmul_mat4f(&(mat4f){.cols = {a, a, a, a}}, &ID4F);
-    print_mat4f(&A);
-
-/*    mat4 A;
-    vec4 v;
-    vec4 b = mvmul4f(A, v);
-    mat4 B = mmul4f(A,A);
-    vec c = vmul4f()
-    vec4 u = dot4f(v, b);
-    vec4 t = red4f(v);
-    vec4 v = vadd4f()
-    madd4f
-*/
-
+    A = matmul4f(A, id4f());
+    print_mat4f(A);
 
     return 0;
 }
--- c/tinymath.h
+++ c/tinymath.h
@@ -1,34 +1,159 @@
 #ifndef TINYMATH_H
 #define TINYMATH_H
 
-typedef union {
+#include <math.h>
+
+/* * * * * * * * *
+ *     vec3f     *
+ * * * * * * * * */
+union vec3f{
+    struct { float x, y, z; };
+    float elt[3];
+};
+typedef union vec3f vec3f;
+
+
+/* * * * * * * * *
+ *     vec4f     *
+ * * * * * * * * */
+union vec4f{
     struct { float x, y, z, w; };
-    float el[4];
-} vec4f;
+    float elt[4];
+};
+typedef union vec4f vec4f;
 
-typedef union {
+static inline vec3f xyz(vec4f u) {
+    return (vec3f) {u.x, u.y, u.z};
+}
+
+static inline vec4f vadd4f(vec4f u, vec4f v) {
+    return (vec4f) {u.x + v.x, u.y + v.y, u.z + v.z, u.w + v.w};
+}
+
+static inline vec4f vsub4f(vec4f u, vec4f v) {
+    return (vec4f) {u.x - v.x, u.y - v.y, u.z - v.z, u.w - v.w};
+}
+
+static inline vec4f vmul4f(vec4f u, vec4f v) {
+    return (vec4f) {u.x * v.x, u.y * v.y, u.z * v.z, u.w * v.w};
+}
+
+static inline vec4f vdiv4f(vec4f u, vec4f v) {
+    return (vec4f) {u.x / v.x, u.y / v.y, u.z / v.z, u.w / v.w};
+}
+
+static inline vec4f vsmul4f(vec4f u, float s) {
+    return (vec4f) {u.x * s, u.y * s, u.z * s, u.w * s};
+}
+
+static inline vec4f vsdiv4f(vec4f u, float s) {
+    return (vec4f) {u.x / s, u.y / s, u.z / s, u.w / s};
+}
+
+static inline float redu4f(vec4f u) {
+    return u.x + u.y + u.z + u.w;
+}
+
+static inline float dot4f(vec4f u, vec4f v) {
+    return redu4f(vmul4f(u, v));
+}
+
+static inline float len4f(vec4f u) {
+    return sqrtf(redu4f(vmul4f(u, u)));
+}
+
+static inline vec4f unit4f(vec4f u) {
+    return vsdiv4f(u, len4f(u));
+}
+
+static inline float dist4f(vec4f u, vec4f v) {
+    return len4f(vsub4f(u, v));
+}
+
+/* * * * * * * * *
+ *     mat4f     *
+ * * * * * * * * */
+union mat4f{
     vec4f col[4];
-    float el[16];
-} mat4f;
+    float elt[16];
+};
+typedef union mat4f mat4f;
 
-static inline vec4f addvec4f(vec4f u, vec4f v); // vadd4f
-static inline vec4f subvec4f(vec4f u, vec4f v); // vsub4f
-static inline vec4f mulvec4f(vec4f u, vec4f v); // vmul4f
-static inline vec4f divvec4f(vec4f u, vec4f v); // vdiv4f
-static inline vec4f smulvec4f(vec4f u, float s); // vsmul4f
-static inline vec4f sdivvec4f(vec4f u, float s); // vsdiv4f
-static inline float dot4f(vec4f u, vec4f v);
-static inline float len4f(vec4f u);
-static inline float dist4f(vec4f u, vec4f v);
-static inline vec4f unit4f(vec4f u);
+static inline mat4f madd4f(mat4f A, mat4f B) {
+    return (mat4f) {
+        vadd4f(A.col[0], B.col[0]),
+        vadd4f(A.col[1], B.col[1]),
+        vadd4f(A.col[2], B.col[2]),
+        vadd4f(A.col[3], B.col[3])
+    };
+}
 
-static inline mat4f addmat4f(mat4f A, mat4f B); // madd4f
-static inline mat4f submat4f(mat4f A, mat4f B); // msub4f
-static inline mat4f mulmat4f(mat4f A, mat4f B); // mmul4f
-static inline vec4f matvec4f(mat4f A, vec4f u); // mvmul4f
-static inline mat4f smulmat4f(mat4f A, float s); // msmul4f
-static inline mat4f sdivmat4f(mat4f A, float s); // msdiv4f
-static inline mat4f trans4f(mat4f A);
+static inline mat4f msub4f(mat4f A, mat4f B) {
+    return (mat4f) {
+        vsub4f(A.col[0], B.col[0]),
+        vsub4f(A.col[1], B.col[1]),
+        vsub4f(A.col[2], B.col[2]),
+        vsub4f(A.col[3], B.col[3])
+    };
+}
+
+static inline mat4f mhad4f(mat4f A, mat4f B) {
+    return (mat4f) {
+        vmul4f(A.col[0], B.col[0]),
+        vmul4f(A.col[1], B.col[1]),
+        vmul4f(A.col[2], B.col[2]),
+        vmul4f(A.col[3], B.col[3])
+    };
+}
+
+static inline vec4f mvmul4f(mat4f A, vec4f u) {
+    vec4f r =     vsmul4f(A.col[0], u.elt[0]);
+    r = vadd4f(r, vsmul4f(A.col[1], u.elt[1]));
+    r = vadd4f(r, vsmul4f(A.col[2], u.elt[2]));
+    r = vadd4f(r, vsmul4f(A.col[3], u.elt[3]));
+    return r;
+}
+
+static inline mat4f matmul4f(mat4f A, mat4f B) {
+    return (mat4f) {
+        mvmul4f(A, B.col[0]),
+        mvmul4f(A, B.col[1]),
+        mvmul4f(A, B.col[2]),
+        mvmul4f(A, B.col[3]),
+    };
+}
+
+static inline mat4f msmul4f(mat4f A, float s) {
+    return (mat4f) {
+        vsmul4f(A.col[0], s),
+        vsmul4f(A.col[1], s),
+        vsmul4f(A.col[2], s),
+        vsmul4f(A.col[3], s)
+    };
+}
+
+static inline mat4f msdiv4f(mat4f A, float s) {
+    return (mat4f) {
+        vsdiv4f(A.col[0], s),
+        vsdiv4f(A.col[1], s),
+        vsdiv4f(A.col[2], s),
+        vsdiv4f(A.col[3], s)
+    };
+}
+
+static inline mat4f transp4f(mat4f A) {
+    vec4f row0 = {A.elt[0], A.elt[4], A.elt[8],  A.elt[12]};
+    vec4f row1 = {A.elt[1], A.elt[5], A.elt[9],  A.elt[13]};
+    vec4f row2 = {A.elt[2], A.elt[6], A.elt[10], A.elt[14]};
+    vec4f row3 = {A.elt[3], A.elt[7], A.elt[11], A.elt[15]};
+    return (mat4f) {row0, row1, row2, row3};
+}
+
+static inline mat4f id4f(void) {
+    mat4f R = {0};
+    R.elt[0] = R.elt[5] = R.elt[10] = R.elt[15] = 1.0f;
+    return R;
+}
 
 
 #endif // TINYMATH_H
