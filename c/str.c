93bc82023d850aa6aa82d9074311f68d29858200
Mateus Orsoni <matorsoni45@gmail.com>
2025-03-28 09:40:25 -0300

    Fix missing braces causing compilation errors on vector math

--- c/str.c
+++ c/str.c
@@ -5,47 +5,49 @@
 #include <stdlib.h>
 
 struct str_t {
-    char*  str;
+    char*  dat;
     size_t len;
 };
 typedef struct str_t str_t;
 
-// charray must be a stack allocated string
-#define static_str(charray) \
-    (str_t){.str = charray, .len = sizeof(charray) - 1}
-
-void str_replace(str_t s, char key, char new) {
-    for (size_t i = 0; i < s.len; i++) {
-        if (s.str[i] == key) s.str[i] = new;
-    }
-}
-
 struct strbuf_t {
-    char*  str;
+    char*  dat;
     size_t len;
     size_t cap;
 };
 typedef struct strbuf_t strbuf_t;
 
-str_t as_str(strbuf_t sb) {
-    return (str_t) {.str = sb.str, .len = sb.len};
+// charray must be a stack allocated string
+#define static_str(charray) \
+    (str_t){.dat = charray, .len = sizeof(charray) - 1}
+
+void str_replace(str_t s, char key, char new) {
+    for (size_t i = 0; i < s.len; i++) {
+        if (s.dat[i] == key) s.dat[i] = new;
+    }
 }
 
-void strbuf_reserve(strbuf_t* sb, size_t new_cap);
+str_t sbf_as_str(strbuf_t sb) {
+    return (str_t) {.dat = sb.dat, .len = sb.len};
+}
 
-strbuf_t strbuf_new(const char* str, size_t length) {
+void sbf_reserve(strbuf_t* sb, size_t new_cap);
+
+strbuf_t sbf_new(const char* str, size_t length) {
     size_t cap = length + 1;
     size_t len = (str == NULL) ? 0 : length;
     char* s = malloc(cap);
     if (len > 0) memcpy(s, str, len);
     s[len] = '\0';
-    return (strbuf_t) {.str = s, .cap = cap, .len = len};
+    return (strbuf_t) {.dat = s, .cap = cap, .len = len};
 }
 
-bool valid_strbuf(const strbuf_t sb) {
-    bool valid_state_1 = sb.str == NULL && (sb.len == sb.cap == 0);
-    bool valid_state_2 = sb.str != NULL && sb.str[sb.len] == '\0' && sb.cap > sb.len;
-    return valid_state_1 || valid_state_2;
+bool sbf_valid(const strbuf_t sb) {
+    bool valid_state[] = {
+        sb.dat == NULL && (sb.len == sb.cap == 0),
+        sb.dat != NULL && sb.dat[sb.len] == '\0' && sb.cap > sb.len
+    };
+    return valid_state[0] || valid_state[1];
 }
 
 
@@ -53,13 +55,13 @@ int main(void) {
     char cu[] = "hello";
     str_t s = static_str(cu);
     printf("String size: %zu\n", s.len);
-    for (int i=0; i < s.len; i++) printf("%c\n", s.str[i]);
+    for (int i = 0; i < s.len; i++) printf("%c\n", s.dat[i]);
 
-    strbuf_t sb = strbuf_new(s.str, s.len);
-    printf("%s\n", sb.str);
+    strbuf_t sb = sbf_new(s.dat, s.len);
+    printf("%s\n", sb.dat);
 
-    str_replace(as_str(sb), 'l', 'X');
-    printf("%s\n", sb.str);
+    str_replace(sbf_as_str(sb), 'l', 'X');
+    printf("%s\n", sb.dat);
 
     return 0;
 }
--- c/tinymath.h
+++ c/tinymath.h
@@ -13,27 +13,27 @@ union vec2f {
 typedef union vec2f vec2f;
 
 static inline vec2f vadd2f(vec2f u, vec2f v) {
-    return (vec2f) {u.x + v.x, u.y + v.y};
+    return (vec2f) { {u.x + v.x, u.y + v.y} };
 }
 
 static inline vec2f vsub2f(vec2f u, vec2f v) {
-    return (vec2f) {u.x - v.x, u.y - v.y};
+    return (vec2f) { {u.x - v.x, u.y - v.y} };
 }
 
 static inline vec2f vmul2f(vec2f u, vec2f v) {
-    return (vec2f) {u.x * v.x, u.y * v.y};
+    return (vec2f) { {u.x * v.x, u.y * v.y} };
 }
 
 static inline vec2f vdiv2f(vec2f u, vec2f v) {
-    return (vec2f) {u.x / v.x, u.y / v.y};
+    return (vec2f) { {u.x / v.x, u.y / v.y} };
 }
 
 static inline vec2f vsmul2f(vec2f u, float s) {
-    return (vec2f) {u.x * s, u.y * s};
+    return (vec2f) { {u.x * s, u.y * s} };
 }
 
 static inline vec2f vsdiv2f(vec2f u, float s) {
-    return (vec2f) {u.x / s, u.y / s};
+    return (vec2f) { {u.x / s, u.y / s} };
 }
 
 static inline float redu2f(vec2f u) {
@@ -66,24 +66,24 @@ union mat2f {
 typedef union mat2f mat2f;
 
 static inline mat2f madd2f(mat2f A, mat2f B) {
-    return (mat2f) {
+    return (mat2f) {{
         vadd2f(A.col[0], B.col[0]),
         vadd2f(A.col[1], B.col[1])
-    };
+    }};
 }
 
 static inline mat2f msub2f(mat2f A, mat2f B) {
-    return (mat2f) {
+    return (mat2f) {{
         vsub2f(A.col[0], B.col[0]),
         vsub2f(A.col[1], B.col[1])
-    };
+    }};
 }
 
 static inline mat2f mhad2f(mat2f A, mat2f B) {
-    return (mat2f) {
+    return (mat2f) {{
         vmul2f(A.col[0], B.col[0]),
         vmul2f(A.col[1], B.col[1])
-    };
+    }};
 }
 
 static inline vec2f mvmul2f(mat2f A, vec2f u) {
@@ -93,30 +93,30 @@ static inline vec2f mvmul2f(mat2f A, vec2f u) {
 }
 
 static inline mat2f matmul2f(mat2f A, mat2f B) {
-    return (mat2f) {
+    return (mat2f) {{
         mvmul2f(A, B.col[0]),
         mvmul2f(A, B.col[1])
-    };
+    }};
 }
 
 static inline mat2f msmul2f(mat2f A, float s) {
-    return (mat2f) {
+    return (mat2f) {{
         vsmul2f(A.col[0], s),
         vsmul2f(A.col[1], s)
-    };
+    }};
 }
 
 static inline mat2f msdiv2f(mat2f A, float s) {
-    return (mat2f) {
+    return (mat2f) {{
         vsdiv2f(A.col[0], s),
         vsdiv2f(A.col[1], s)
-    };
+    }};
 }
 
 static inline mat2f transp2f(mat2f A) {
-    vec2f row0 = {A.elt[0], A.elt[2]};
-    vec2f row1 = {A.elt[1], A.elt[3]};
-    return (mat2f) {row0, row1};
+    vec2f row0 = {{A.elt[0], A.elt[2]}};
+    vec2f row1 = {{A.elt[1], A.elt[3]}};
+    return (mat2f) {{row0, row1}};
 }
 
 static inline mat2f id2f(void) {
@@ -136,31 +136,31 @@ union vec3f {
 typedef union vec3f vec3f;
 
 static inline vec2f xy(vec3f u) {
-    return (vec2f) {u.x, u.y};
+    return (vec2f) { {u.x, u.y} };
 }
 
 static inline vec3f vadd3f(vec3f u, vec3f v) {
-    return (vec3f) {u.x + v.x, u.y + v.y, u.z + v.z};
+    return (vec3f) { {u.x + v.x, u.y + v.y, u.z + v.z} };
 }
 
 static inline vec3f vsub3f(vec3f u, vec3f v) {
-    return (vec3f) {u.x - v.x, u.y - v.y, u.z - v.z};
+    return (vec3f) { {u.x - v.x, u.y - v.y, u.z - v.z} };
 }
 
 static inline vec3f vmul3f(vec3f u, vec3f v) {
-    return (vec3f) {u.x * v.x, u.y * v.y, u.z * v.z};
+    return (vec3f) { {u.x * v.x, u.y * v.y, u.z * v.z} };
 }
 
 static inline vec3f vdiv3f(vec3f u, vec3f v) {
-    return (vec3f) {u.x / v.x, u.y / v.y, u.z / v.z};
+    return (vec3f) { {u.x / v.x, u.y / v.y, u.z / v.z} };
 }
 
 static inline vec3f vsmul3f(vec3f u, float s) {
-    return (vec3f) {u.x * s, u.y * s, u.z * s};
+    return (vec3f) { {u.x * s, u.y * s, u.z * s} };
 }
 
 static inline vec3f vsdiv3f(vec3f u, float s) {
-    return (vec3f) {u.x / s, u.y / s, u.z / s};
+    return (vec3f) { {u.x / s, u.y / s, u.z / s} };
 }
 
 static inline float redu3f(vec3f u) {
@@ -193,27 +193,27 @@ union mat3f {
 typedef union mat3f mat3f;
 
 static inline mat3f madd3f(mat3f A, mat3f B) {
-    return (mat3f) {
+    return (mat3f) {{
         vadd3f(A.col[0], B.col[0]),
         vadd3f(A.col[1], B.col[1]),
         vadd3f(A.col[2], B.col[2])
-    };
+    }};
 }
 
 static inline mat3f msub3f(mat3f A, mat3f B) {
-    return (mat3f) {
+    return (mat3f) {{
         vsub3f(A.col[0], B.col[0]),
         vsub3f(A.col[1], B.col[1]),
         vsub3f(A.col[2], B.col[2])
-    };
+    }};
 }
 
 static inline mat3f mhad3f(mat3f A, mat3f B) {
-    return (mat3f) {
+    return (mat3f) {{
         vmul3f(A.col[0], B.col[0]),
         vmul3f(A.col[1], B.col[1]),
         vmul3f(A.col[2], B.col[2])
-    };
+    }};
 }
 
 static inline vec3f mvmul3f(mat3f A, vec3f u) {
@@ -224,34 +224,34 @@ static inline vec3f mvmul3f(mat3f A, vec3f u) {
 }
 
 static inline mat3f matmul3f(mat3f A, mat3f B) {
-    return (mat3f) {
+    return (mat3f) {{
         mvmul3f(A, B.col[0]),
         mvmul3f(A, B.col[1]),
         mvmul3f(A, B.col[2])
-    };
+    }};
 }
 
 static inline mat3f msmul3f(mat3f A, float s) {
-    return (mat3f) {
+    return (mat3f) {{
         vsmul3f(A.col[0], s),
         vsmul3f(A.col[1], s),
         vsmul3f(A.col[2], s)
-    };
+    }};
 }
 
 static inline mat3f msdiv3f(mat3f A, float s) {
-    return (mat3f) {
+    return (mat3f) {{
         vsdiv3f(A.col[0], s),
         vsdiv3f(A.col[1], s),
         vsdiv3f(A.col[2], s)
-    };
+    }};
 }
 
 static inline mat3f transp3f(mat3f A) {
-    vec3f row0 = {A.elt[0], A.elt[3], A.elt[6]};
-    vec3f row1 = {A.elt[1], A.elt[4], A.elt[7]};
-    vec3f row2 = {A.elt[2], A.elt[5], A.elt[8]};
-    return (mat3f) {row0, row1, row2};
+    vec3f row0 = {{A.elt[0], A.elt[3], A.elt[6]}};
+    vec3f row1 = {{A.elt[1], A.elt[4], A.elt[7]}};
+    vec3f row2 = {{A.elt[2], A.elt[5], A.elt[8]}};
+    return (mat3f) {{row0, row1, row2}};
 }
 
 static inline mat3f id3f(void) {
@@ -272,31 +272,31 @@ union vec4f {
 typedef union vec4f vec4f;
 
 static inline vec3f xyz(vec4f u) {
-    return (vec3f) {u.x, u.y, u.z};
+    return (vec3f) { {u.x, u.y, u.z} };
 }
 
 static inline vec4f vadd4f(vec4f u, vec4f v) {
-    return (vec4f) {u.x + v.x, u.y + v.y, u.z + v.z, u.w + v.w};
+    return (vec4f) { {u.x + v.x, u.y + v.y, u.z + v.z, u.w + v.w} };
 }
 
 static inline vec4f vsub4f(vec4f u, vec4f v) {
-    return (vec4f) {u.x - v.x, u.y - v.y, u.z - v.z, u.w - v.w};
+    return (vec4f) { {u.x - v.x, u.y - v.y, u.z - v.z, u.w - v.w} };
 }
 
 static inline vec4f vmul4f(vec4f u, vec4f v) {
-    return (vec4f) {u.x * v.x, u.y * v.y, u.z * v.z, u.w * v.w};
+    return (vec4f) { {u.x * v.x, u.y * v.y, u.z * v.z, u.w * v.w} };
 }
 
 static inline vec4f vdiv4f(vec4f u, vec4f v) {
-    return (vec4f) {u.x / v.x, u.y / v.y, u.z / v.z, u.w / v.w};
+    return (vec4f) { {u.x / v.x, u.y / v.y, u.z / v.z, u.w / v.w} };
 }
 
 static inline vec4f vsmul4f(vec4f u, float s) {
-    return (vec4f) {u.x * s, u.y * s, u.z * s, u.w * s};
+    return (vec4f) { {u.x * s, u.y * s, u.z * s, u.w * s} };
 }
 
 static inline vec4f vsdiv4f(vec4f u, float s) {
-    return (vec4f) {u.x / s, u.y / s, u.z / s, u.w / s};
+    return (vec4f) { {u.x / s, u.y / s, u.z / s, u.w / s} };
 }
 
 static inline float redu4f(vec4f u) {
@@ -330,30 +330,30 @@ union mat4f {
 typedef union mat4f mat4f;
 
 static inline mat4f madd4f(mat4f A, mat4f B) {
-    return (mat4f) {
+    return (mat4f) {{
         vadd4f(A.col[0], B.col[0]),
         vadd4f(A.col[1], B.col[1]),
         vadd4f(A.col[2], B.col[2]),
         vadd4f(A.col[3], B.col[3])
-    };
+    }};
 }
 
 static inline mat4f msub4f(mat4f A, mat4f B) {
-    return (mat4f) {
+    return (mat4f) {{
         vsub4f(A.col[0], B.col[0]),
         vsub4f(A.col[1], B.col[1]),
         vsub4f(A.col[2], B.col[2]),
         vsub4f(A.col[3], B.col[3])
-    };
+    }};
 }
 
 static inline mat4f mhad4f(mat4f A, mat4f B) {
-    return (mat4f) {
+    return (mat4f) {{
         vmul4f(A.col[0], B.col[0]),
         vmul4f(A.col[1], B.col[1]),
         vmul4f(A.col[2], B.col[2]),
         vmul4f(A.col[3], B.col[3])
-    };
+    }};
 }
 
 static inline vec4f mvmul4f(mat4f A, vec4f u) {
@@ -365,38 +365,38 @@ static inline vec4f mvmul4f(mat4f A, vec4f u) {
 }
 
 static inline mat4f matmul4f(mat4f A, mat4f B) {
-    return (mat4f) {
+    return (mat4f) {{
         mvmul4f(A, B.col[0]),
         mvmul4f(A, B.col[1]),
         mvmul4f(A, B.col[2]),
         mvmul4f(A, B.col[3]),
-    };
+    }};
 }
 
 static inline mat4f msmul4f(mat4f A, float s) {
-    return (mat4f) {
+    return (mat4f) {{
         vsmul4f(A.col[0], s),
         vsmul4f(A.col[1], s),
         vsmul4f(A.col[2], s),
         vsmul4f(A.col[3], s)
-    };
+    }};
 }
 
 static inline mat4f msdiv4f(mat4f A, float s) {
-    return (mat4f) {
+    return (mat4f) {{
         vsdiv4f(A.col[0], s),
         vsdiv4f(A.col[1], s),
         vsdiv4f(A.col[2], s),
         vsdiv4f(A.col[3], s)
-    };
+    }};
 }
 
 static inline mat4f transp4f(mat4f A) {
-    vec4f row0 = {A.elt[0], A.elt[4], A.elt[8],  A.elt[12]};
-    vec4f row1 = {A.elt[1], A.elt[5], A.elt[9],  A.elt[13]};
-    vec4f row2 = {A.elt[2], A.elt[6], A.elt[10], A.elt[14]};
-    vec4f row3 = {A.elt[3], A.elt[7], A.elt[11], A.elt[15]};
-    return (mat4f) {row0, row1, row2, row3};
+    vec4f row0 = {{A.elt[0], A.elt[4], A.elt[8],  A.elt[12]}};
+    vec4f row1 = {{A.elt[1], A.elt[5], A.elt[9],  A.elt[13]}};
+    vec4f row2 = {{A.elt[2], A.elt[6], A.elt[10], A.elt[14]}};
+    vec4f row3 = {{A.elt[3], A.elt[7], A.elt[11], A.elt[15]}};
+    return (mat4f) {{row0, row1, row2, row3}};
 }
 
 static inline mat4f id4f(void) {
