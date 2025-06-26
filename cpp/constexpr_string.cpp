f0cc0603a96bc83f9014b4a469278f793c0dbbdc
Mateus Orsoni <matorsoni45@gmail.com>
2025-06-26 20:02:48 -0300

    Add C++ constexpr strings experiment

---
+++ cpp/constexpr_string.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <array>
+#include <cstddef> // for std::size_t
+
+template<std::size_t N>
+using charray = std::array<char, N>;
+
+template<std::size_t N>
+constexpr auto to_charray(const char (&str)[N]) {
+    charray<N> result{};
+    for (std::size_t i = 0; i < N; ++i) {
+        result[i] = str[i];
+    }
+    return result;
+}
+
+template<std::size_t N1, std::size_t N2>
+constexpr auto concat(const charray<N1>& s1, const charray<N2>& s2) {
+    charray<N1 + N2 - 1> result{};
+    for (std::size_t i = 0; i < N1 - 1; ++i) {
+        result[i] = s1[i];
+    }
+    for(std::size_t i = 0; i < N2; ++i) {
+        result[N1 - 1 + i] = s2[i];
+    }
+    return result;
+}
+
+constexpr auto who = to_charray(R"(0123456789)");
+static_assert(sizeof(who) == 11);
+
+constexpr auto s1 = to_charray(R"(Hello, )");
+constexpr auto s2 = to_charray(R"(constexpr strings!)");
+
+int main() {
+    std::cout << concat(s1,s2).data() << "\n";
+    printf("%s\n", who.data());
+}
